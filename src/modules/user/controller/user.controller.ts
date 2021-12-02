605439189551ec76c9a85377e88dbe7e2ce9e01d
hongocthaotrang <trangho.2632@gmail.com>
2021-12-02 11:05:32 +0700

    init feature register

--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiBody, ApiCreatedResponse, ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
+import { IUser } from '../domain/interfaces/IUser.interface';
+import { CreateUserDto, UserResponseDto } from '../dto/user.dto';
 import { UserService } from '../service/user.service';
 
+@ApiTags("User")
 @Controller('user')
 export class UserController {
-  constructor(private readonly appService: UserService) {}
+  constructor(private userServices: UserService) {
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post("/register")
+  @ApiCreatedResponse({
+    description: "The record has been successfully created.",
+    type: UserResponseDto
+  })
+  @ApiForbiddenResponse({
+    description: "Forbidden."
+  })
+  @ApiBody({ type: CreateUserDto })
+  async createUser(
+    @Body() userInfo: CreateUserDto
+  ): Promise<UserResponseDto> {
+    
+    return this.userServices.createUser(userInfo);
   }
 }
--- src/modules/user/service/user.service.ts
+++ src/modules/user/service/user.service.ts
@@ -1,8 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RoleRepository } from 'src/modules/role/infrastructure/role.repository';
+import { IUser } from '../domain/interfaces/IUser.interface';
+import { CreateUserDto, UserResponseDto } from '../dto/user.dto';
+import { UserRepository } from '../infrastructure/user.repository';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class UserService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor( 
+    @InjectRepository(UserRepository)
+    private readonly userRepository: UserRepository, 
+    @InjectRepository(RoleRepository)
+    private readonly roleRepository: RoleRepository 
+  ) {}
+
+  
+
+  async createUser(userInfo:CreateUserDto): Promise<UserResponseDto> {
+    const roleUserId = await (await this.roleRepository.findOne({name: "User"})).id;  
+    if(!roleUserId){
+      throw new UnauthorizedException('Cant find user id ')
+    }  
+    const saltOrRounds = 10;
+    const newUser: IUser = {
+      email: userInfo.email,
+      username: userInfo.email,
+      password: await bcrypt.hash(userInfo.password, saltOrRounds),
+      birthday: userInfo.birthday,
+      name:  userInfo.name,
+      roleId: roleUserId,
+    };
+    try {
+      const user = await this.userRepository.save(newUser);
+      if (!user) throw new ForbiddenException();
+      const { id, password, ...result } = user; 
+      return result;
+    } catch (err) {
+      throw new UnauthorizedException("Email has been used")
+    }
   }
 }
