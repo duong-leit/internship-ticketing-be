c61509d6a0ab147abccec4cdf488f398aa937e4c
duong-leit <duong.le@zodinet.tech>
2021-12-02 14:27:08 +0700

    update role for login response

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -5,17 +5,25 @@ import { UserLoginDto } from '../infrastructure/dto/systemLogin.dto';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { responseLoginDto } from '../infrastructure/dto/responseLogin.dto';
+import { RoleRepository } from 'src/modules/role/infrastructure/role.repository';
+
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository, private jwtService: JwtService) {}
+  constructor(private userRepository: UserRepository, private jwtService: JwtService, private roleRepository: RoleRepository) {}
   async systemLogin(data: UserLoginDto): Promise<responseLoginDto> {
-    const result = await this.userRepository.findOne({ username: data.username });
+    const result = await this.userRepository.findOne({
+      relations: ['role'],
+      where: {
+        username: data.username,
+      },
+    });
     if (!result) throw new UnauthorizedException('username is invalid');
     if (bcrypt.compareSync(data.password, result.password)) {
       const payload = {
         sub: result.id,
         name: result.name,
         avatar: result.avatar,
+        role: result.role.name,
       };
       return {
         token: this.jwtService.sign(payload),
