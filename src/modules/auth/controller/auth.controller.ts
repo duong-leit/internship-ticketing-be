f338ee2a54163558e15443c18aa1e2c212851166
duong-leit <duong.le@zodinet.tech>
2021-12-03 21:54:16 +0700

    create facebook login api

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,5 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiBody, ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { FacebookLoginDto } from '../infrastructure/dto/facebookLoginDto';
 import { responseLoginDto } from '../infrastructure/dto/responseLogin.dto';
 import { UserLoginDto } from '../infrastructure/dto/systemLogin.dto';
 import { AuthService } from '../service/auth.service';
@@ -15,5 +22,14 @@ export class AuthController {
   @ApiBody({ type: UserLoginDto })
   login(@Body() user: UserLoginDto): Promise<responseLoginDto> {
     return this.authService.systemLogin(user);
+  }
+
+  @Post('fb-login')
+  @ApiOkResponse({ type: responseLoginDto })
+  @ApiUnauthorizedResponse({ status: 401, description: 'user is invalid' })
+  @ApiForbiddenResponse({ status: 403, description: 'Email is already used for another account' })
+  @ApiBody({ type: FacebookLoginDto })
+  facebookLogin(@Body() user: FacebookLoginDto): Promise<responseLoginDto> {
+    return this.authService.facebookLogin(user);
   }
 }
--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,32 +1,86 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserRepository } from 'src/modules/user/infrastructure/user.repository';
 import { UserLoginDto } from '../infrastructure/dto/systemLogin.dto';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { responseLoginDto } from '../infrastructure/dto/responseLogin.dto';
+import { FacebookLoginDto } from '../infrastructure/dto/facebookLoginDto';
+import { FacebookAuthService } from 'facebook-auth-nestjs';
+import { RoleRepository } from 'src/modules/role/infrastructure/role.repository';
+import { IFacebookData } from '../infrastructure/interface/facebook.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository, private jwtService: JwtService) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+    private readonly fbService: FacebookAuthService,
+    private roleRepository: RoleRepository
+  ) {}
+
   async systemLogin(data: UserLoginDto): Promise<responseLoginDto> {
-    const result = await this.userRepository.findOne({
+    const isExistUser = await this.userRepository.findOne({
       relations: ['role'],
-      where: {
-        username: data.username,
-      },
+      where: { username: data.username, isSocial: false },
     });
-    if (!result) throw new UnauthorizedException('username is invalid');
-    if (bcrypt.compareSync(data.password, result.password)) {
-      const payload = {
-        sub: result.id,
-        name: result.name,
-        avatar: result.avatar,
-        role: result.role.name,
-      };
-      return {
-        token: this.jwtService.sign(payload),
-      };
+    if (!isExistUser) throw new UnauthorizedException('username is invalid');
+    if (!bcrypt.compareSync(data.password, isExistUser.password)) {
+      throw new UnauthorizedException('password is invalid');
     }
-    throw new UnauthorizedException('password is invalid');
+    return this.generateJWTToken(isExistUser);
+  }
+
+  async facebookLogin({ accessToken }: FacebookLoginDto): Promise<responseLoginDto> {
+    const userInformation: IFacebookData = await this.fbService.getUser(
+      accessToken,
+      'id',
+      'name',
+      'email',
+      'birthday'
+    );
+
+    if (!userInformation) throw new UnauthorizedException('user is invalid');
+    let existUser = await this.userRepository.findOne({
+      relations: ['role'],
+      where: { username: userInformation.id, isSocial: true },
+    });
+
+    if (!existUser) {
+      existUser = await this.userRepository.findOne({
+        where: { email: userInformation.email },
+      });
+
+      if (existUser) {
+        throw new ForbiddenException('Email is already used');
+      }
+      const roleUser = await this.roleRepository.findOne({ name: 'User' });
+      if (!roleUser) throw new InternalServerErrorException();
+
+      existUser = await this.userRepository.save({
+        name: userInformation.name,
+        username: userInformation.id,
+        email: userInformation.email || null,
+        birthday: userInformation.birthday || null,
+        isSocial: true,
+        password: '',
+        role: roleUser || null,
+      });
+    }
+    return this.generateJWTToken(existUser);
+  }
+
+  generateJWTToken(data: any): responseLoginDto {
+    const payload = {
+      sub: data.id,
+      name: data.name,
+      avatar: data.avatar,
+      role: data.role.name,
+    };
+    return { token: this.jwtService.sign(payload) };
   }
 }
