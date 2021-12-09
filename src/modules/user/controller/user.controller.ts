6b704ddae874c4673d0f5ae97560a75dba8bd0b0
Khoa <khoa.ha@zodinet.tech>
2021-12-09 09:53:51 +0700

    fix user api

--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, Res } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiBody,
@@ -9,18 +9,47 @@ import {
 import { Recaptcha } from '@nestlab/google-recaptcha';
 import {
   CreateFacebookUserDto,
-  CreateSystemUserDto,
+  CreateSystemUserDto, GetListUserDto,
   UserResponseDto,
 } from '../dto/user.dto';
 import { UserService } from '../service/user.service';
+import { transferResponse } from '../../../common/utils/transferResponse';
+import {Response} from 'express';
+
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private userServices: UserService) {}
 
-  @Recaptcha({ action: 'register' })
+  @Post()
+  @ApiBody({
+    type: GetListUserDto
+  })
+  @ApiBadRequestResponse()
+  async getUserPaging(
+    @Param() param: {id: string},
+    @Body() data: {
+      filter:{
+        [key: string]: string | number
+      },
+      pagination: {
+        pageSize: number, pageIndex: number
+      }
+    },
+    @Res() res: Response
+  ){
+    const response = await this.userServices.getListUser(
+      {...data.filter},
+      {arrayRelation: ['role']},
+      {pageSize: data.pagination.pageSize,
+        pageIndex: data.pagination.pageIndex},
+    )
+    transferResponse(res, response)
+  }
+
   @Post('/register')
+  @Recaptcha({ action: 'register' })
   @ApiCreatedResponse({
     description: 'The record has been successfully created.',
     type: UserResponseDto,
